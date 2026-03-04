ee4fa6c6f342e104d73e8d1daf3bf364aaf4d82c
Riya206 <aryadevi332@gmail.com>
2026-03-04 21:48:55 +0530

    Commit

---
+++ Pattern_problems/src/com/kce/pattern/Number_Repeated_Triangle_Pattern.java
@@ -0,0 +1,19 @@
+package com.kce.pattern;
+
+import java.util.Scanner;
+
+public class Number_Repeated_Triangle_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(" "+i);
+			}
+			System.out.println(" ");
+		}
+			}
+}
---
+++ Pattern_problems/src/com/kce/pattern/Right_Angled_Number_Triangle.java
@@ -0,0 +1,20 @@
+package com.kce.pattern;
+
+import java.util.Scanner;
+
+public class Right_Angled_Number_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a Number:");
+int n=sc.nextInt();
+for(int i=1;i<=n;i++) {
+	for(int j=1;j<=i;j++) {
+		System.out.print(" "+j);
+	}
+	System.out.println(" ");
+}
+	}
+
+}
