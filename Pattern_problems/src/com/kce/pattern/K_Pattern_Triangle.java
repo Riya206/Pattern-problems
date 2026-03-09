39a71c4640370fa520dea42fb3b341f3546af2d8
Riya206 <aryadevi332@gmail.com>
2026-03-09 09:17:55 +0530

    Commit

---
+++ Pattern_problems/src/com/kce/pattern/K_Pattern_Triangle.java
@@ -0,0 +1,26 @@
+package com.kce.pattern;
+
+import java.util.Scanner;
+
+public class K_Pattern_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		 System.out.print("Enter a number");
+		 int n= sc.nextInt();
+		 for(int i=1;i<=n;i++) {
+			 for(int j=1;j<=n-i+1;j++) {
+				 System.out.print("*");
+			 }
+			 System.out.println(" ");
+		 }
+		 for(int i=1;i<n;i++) {
+			 for(int j=1;j<=i+1;j++) {
+				 System.out.print("*");
+			 }
+			 System.out.println(" ");
+		 }
+			}
+	}
+
