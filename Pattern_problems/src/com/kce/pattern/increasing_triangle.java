7c0ee56c0b85750de43bd226251b7631ceb49d60
Riya206 <aryadevi332@gmail.com>
2026-05-21 13:56:10 +0530

    commit

--- Pattern_problems/src/com/kce/pattern/increasing_triangle.java
+++ Pattern_problems/src/com/kce/pattern/increasing_triangle.java
@@ -9,8 +9,8 @@ public class increasing_triangle {
  Scanner sc= new Scanner(System.in);
  System.out.print("Enter a number");
  int n= sc.nextInt();
- for(int i=1;i<=n;i++) {
-	 for(int j=1;j<=i;j++) {
+ for(int i=0;i<n;i++) {
+	 for(int j=0;j<=i;j++) {
 		 System.out.print("*");
 	 }
 	 System.out.println(" ");
